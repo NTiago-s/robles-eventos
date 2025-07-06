91e66e00c87ded02d1696ca4a24aadc1a2c6edcc
nicoplacente <nicolasplacente@gmail.com>
2025-07-06 06:50:39 -0300

    add new style

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -1,28 +1,24 @@
 export const FAQs = [
   {
     id: 1,
-    question:
-      "¿Puedo contratar o cancelar los servicios opcionales en cualquier momento?",
-    p1: "Sí, los servicios opcionales pueden contratarse o cancelarse en cualquier momento. Podés elegir uno o varios según tus necesidades, y si contratás todos juntos, accedés a un <strong>descuento exclusivo</strong>.",
-    p2: "Solo abonás el desarrollo de la app o página web como un pago único. Incluye un dominio gratuito del tipo tu-pagina.vercel.app. Más adelante podés contratar mantenimiento o dominio personalizado y profesional si lo deseás.",
+    question: "¿Se puede contratar un solo servicio?",
+    p1: "Sí, podés contratar solo el servicio que necesites. Ya sea la decoración, mozos, DJ, barra de bebidas o cualquier otro, todos los servicios están disponibles de forma individual. Cada uno tiene su tarifa específica, y si luego decidís sumar más servicios, podemos armar un paquete especial a medida.",
   },
   {
     id: 2,
-    question: "¿Hay reembolsos si cancelo el proyecto o un servicio?",
-    p1: "No. El pago por la app o web es sin reembolso: se abona 50% al iniciar y 50% al finalizar. En el caso de los servicios adicionales (mantenimiento, dominio, etc.).",
-    p2: "Podés dejar de pagarlos, darlos de baja o volver a contratarlos en cualquier momento sin penalidad.",
+    question: "¿Hacen precios especiales si contrato varios servicios?",
+    p1: "Sí, ofrecemos descuentos por contratación de servicios completos. Si decidís contratar más de un servicio (catering + barra + DJ) podemos armarte un presupuesto personalizado con una tarifa promocional por el combo. Buscamos siempre adaptarnos a tu evento y a tu presupuesto.",
   },
   {
     id: 3,
-    question: "¿Cuánto tiempo demora el desarrollo de una web o aplicación?",
-    p1: "Depende de la complejidad del proyecto. Una landing page simple puede entregarse en 2 a 5 días hábiles. Un desarrollo más personalizado puede demorar entre 1 y 4 semanas. En casos complejos, el plazo puede ser indeterminado, pero siempre será informado con anticipación.",
-    p2: "El proceso comienza una vez realizado el primer pago del 50%, y no se entrega el producto final ni se habilita el dominio hasta completar el 100% del pago, según lo establecido en los términos y condiciones.",
+    question: "¿Con cuánta anticipación debo reservar?",
+    p1: "Recomendamos reservar con al menos más de 3 meses de anticipación. Esto nos permite coordinar mejor los detalles, asegurar disponibilidad del equipo y garantizar una experiencia de alta calidad. Sin embargo, también atendemos eventos con poca anticipación, según disponibilidad.",
   },
   {
     id: 4,
-    question: "¿Cómo se realiza la contratación y qué métodos de pago aceptan?",
-    p1: "Para comenzar, se definen los requisitos y se realiza un anticipo del 50%. El resto se abona al finalizar luego de ser entregada la página.",
-    p2: "Aceptamos Mercado Pago, Transferencia Bancaria y Criptomonedas.",
+    question:
+      "¿El servicio incluye todo lo necesario o debo contratar cosas aparte?",
+    p1: "Nuestros servicios son integrales, pero también nos adaptamos. Por ejemplo, el catering incluye vajilla básica, el servicio de barra incluye bartenders y bebidas, y la decoración puede incluir mobiliario, luces y arreglos. De todos modos, podemos ajustar lo incluido según tus necesidades o preferencias.",
   },
 ];
 
